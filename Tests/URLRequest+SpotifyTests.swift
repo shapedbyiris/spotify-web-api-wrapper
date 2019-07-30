4113ee31f38686a96d9b4388106f795dec7a6ec9
Ariel Elkin <ariel@shapedbyiris.com>
2019-07-30 16:54:10 +0100

    test for new search result limits

--- Tests/URLRequest+SpotifyTests.swift
+++ Tests/URLRequest+SpotifyTests.swift
@@ -17,7 +17,7 @@ class URLRequestPlusSpotifyTests: XCTestCase { //swiftlint:disable line_length
     func testGeneralSearchRequestBuildsCorrectly() {
         let duranDuranSearchRequest = URLRequest.init(spotifySearch: "duran duran", token: token)
         XCTAssert(duranDuranSearchRequest.httpMethod == "GET")
-        XCTAssert(duranDuranSearchRequest.url == URL(string: "https://api.spotify.com/v1/search?q=duran%20duran&type=playlist,artist,album,track"))
+        XCTAssert(duranDuranSearchRequest.url == URL(string: "https://api.spotify.com/v1/search?q=duran%20duran&type=playlist,artist,album,track&limit=20"))
         XCTAssert(duranDuranSearchRequest.allHTTPHeaderFields != nil)
         XCTAssert(duranDuranSearchRequest.allHTTPHeaderFields!.contains(where: {
             $0.key == "Authorization" && $0.value == "Bearer token"
